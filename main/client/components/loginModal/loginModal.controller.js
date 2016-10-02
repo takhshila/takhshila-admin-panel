0afaff81f87d511ca91f1094c9be748251e5b6c8
Pratik Raj <pratikraj26@gmail.com>
2016-10-02 16:48:28 +0530

    Displaying server error message on client side for registration

--- main/client/components/loginModal/loginModal.controller.js
+++ main/client/components/loginModal/loginModal.controller.js
@@ -23,29 +23,50 @@ angular.module('takhshilaApp')
       $mdDialog.hide();
     };
 
-    $scope.validate = function(form){
-
-    }
-
-    $scope.signUp = function(registerForm){
-      console.log(registerForm);
-      if(registerForm.$invalid){
-        // var el = angular.element("[name='" + registerForm.$name + "']").find('.ng-invalid:visible:first');
-        // var elName = el[0].name;
-        // registerForm[elName].$dirty = true;
-        // registerForm[elName].$pristine = false;
-        // angular.element("[name='" + registerForm.$name + "']").find('.ng-invalid:visible:first').focus();
+    $scope.validate = function(loginForm){
+      if(loginForm.$invalid){
+        var el = angular.element("[name='" + loginForm.$name + "']").find('.ng-invalid:visible:first');
+        var elName = el[0].name;
+        loginForm[elName].$dirty = true;
+        loginForm[elName].$pristine = false;
+        angular.element("[name='" + loginForm.$name + "']").find('.ng-invalid:visible:first').focus();
         return false;
       }else{
         $scope.logging = true;
-        console.log($scope.registerFormData);
-        Auth.createUser($scope.registerFormData)
+        Auth.login($scope.loginFormData)
         .then(function(data){
           $scope.logging = false;
           console.log(data);
         }, function(err){
           $scope.logging = false;
           console.log(err);
+        })
+      }
+    }
+
+    $scope.signUp = function(registerForm){
+      if(registerForm.$invalid){
+        var el = angular.element("[name='" + registerForm.$name + "']").find('.ng-invalid:visible:first');
+        var elName = el[0].name;
+        registerForm[elName].$dirty = true;
+        registerForm[elName].$pristine = false;
+        angular.element("[name='" + registerForm.$name + "']").find('.ng-invalid:visible:first').focus();
+        return false;
+      }else{
+        $scope.logging = true;
+        Auth.createUser($scope.registerFormData)
+        .then(function(data){
+          $scope.logging = false;
+          $scope.closeDialog();
+        }, function(err){
+          $scope.logging = false;
+          for(var error in err.data.errors){
+            registerForm[error].$valid = false;
+            registerForm[error].$invalid = true;
+            registerForm[error].$error.serverError = true;
+            registerForm[error].$error.errorMessage = err.data.errors[error].message;
+            angular.element("[name='" + registerForm.$name + "'] [name='" + error + "']").focus();
+          }
         })
       }
     }
