cb7c05a41beeff98d75fb09eb98b0f6b0cd989d0
Pratik Raj <pratikraj26@gmail.com>
2017-09-26 22:46:32 +0530

    Updated API for updating and deleting bank accounts

--- main/server/api/bankAccount/bankAccount.controller.js
+++ main/server/api/bankAccount/bankAccount.controller.js
@@ -3,6 +3,7 @@
 var _ = require('lodash');
 var BankAccount = require('./bankAccount.model');
 var Bank = require('../bank/bank.model');
+var User = require('../user/user.model');
 
 // Get list of bankAccounts
 exports.getBankAccounts = function(req, res) {
@@ -85,30 +86,90 @@ exports.addBankAccount = function(req, res) {
   })
 };
 
-// Updates an existing bankAccount in the DB.
-exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
-  BankAccount.findById(req.params.id, function (err, bankAccount) {
-    if (err) { return handleError(res, err); }
-    if(!bankAccount) { return res.status(404).send('Not Found'); }
-    var updated = _.merge(bankAccount, req.body);
-    updated.save(function (err) {
-      if (err) { return handleError(res, err); }
-      return res.status(200).json(bankAccount);
+
+exports.updateBankAccount = function(req, res) {
+  var updateBankAccountPromise = new Promise(function(resolve, reject){
+    User.findById(userId, function (err, user){
+      if (err) { reject(err); }
+      BankAccount.findById(req.params.id, function (err, bankAccount){
+        if (err) { reject(err); }
+        var userID = req.user._id;
+        bankAccount.name = req.body.name;
+        bankAccount.accountNumber = req.body.accountNumber;
+        bankAccount.bankName = req.body.bankName;
+        bankAccount.branchName = req.body.branchName;
+        bankAccount.ifscCode = req.body.ifscCode;
+        Bank.findOne({
+          ifsc: req.body.bankDetails.IFSC
+        })
+        .exec(function(err, bank){
+          if(!bank){
+            var bankData = {
+              bank: req.body.bankDetails.BANK,
+              ifsc: req.body.bankDetails.IFSC,
+              branch: req.body.bankDetails.BRANCH,
+              address: req.body.bankDetails.ADDRESS,
+              state: req.body.bankDetails.STATE,
+              district: req.body.bankDetails.DISTRICT,
+              city: req.body.bankDetails.CITY,
+              mirCode: req.body.bankDetails.MICRCODE,
+              contact: req.body.bankDetails.CONTACT,
+            }
+            Bank.create(bankData, function(err, newBank){
+              bankAccount.bankID = newBank._id.toString();
+              bankAccount.save(function(err, bankAccount) {
+                if(err) { reject(err); }
+                resolve(bankAccount);
+              });
+            });
+          }else{
+            bankAccount.bankID = bank._id.toString();
+            bankAccount.save(function(err, bankAccount) {
+              if(err) { reject(err); }
+              resolve(bankAccount);
+            });
+          }
+        });
+      });
     });
   });
+
+  updateBankAccountPromise
+  .then(function(bankAccount){
+    return res.status(201).json(bankAccount);
+  })
+  .catch(function(err){
+    console.log(err);
+    return handleError(res, err);
+  })
 };
 
 // Deletes a bankAccount from the DB.
-exports.destroy = function(req, res) {
-  BankAccount.findById(req.params.id, function (err, bankAccount) {
-    if(err) { return handleError(res, err); }
-    if(!bankAccount) { return res.status(404).send('Not Found'); }
-    bankAccount.remove(function(err) {
-      if(err) { return handleError(res, err); }
-      return res.status(204).send('No Content');
+exports.deleteBankAccount = function(req, res) {
+  var deleteBankAccountPromise = new Promise(function(resolve, reject){
+    var userId = req.user._id;
+    if(req.body._id) { delete req.body._id; }
+    User.findById(userId, function (err, user){
+      if (err) { reject(err); }
+      BankAccount.findById(req.params.id, function (err, bankAccount) {
+        if (err) { reject(err); }
+        if(!bankAccount) { reject('Not Found'); }
+        bankAccount.remove(function(err) {
+          if (err) { reject(err); }
+          resolve('No Content');
+        });
+      });
     });
   });
+
+  deleteBankAccountPromise
+  .then(function(response){
+    return res.status(204).json(response);
+  })
+  .catch(function(err){
+    console.log(err);
+    return handleError(res, err);
+  })
 };
 
 function handleError(res, err) {
--- main/server/api/bankAccount/index.js
+++ main/server/api/bankAccount/index.js
@@ -9,11 +9,11 @@ var router = express.Router();
 
 router.get('/', auth.isAuthenticated(), controller.getBankAccounts);
 router.post('/', auth.isAuthenticated(), controller.addBankAccount);
+router.put('/:id', auth.isAuthenticated(), controller.updateBankAccount);
+router.delete('/:id', auth.isAuthenticated(),controller.deleteBankAccount);
 
 // router.get('/:id', controller.show);
 // router.post('/', controller.create);
-// router.put('/:id', controller.update);
 // router.patch('/:id', controller.update);
-// router.delete('/:id', controller.destroy);
 
 module.exports = router;
