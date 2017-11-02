8dd147b78416d9166e0af42643c762e2b0db737f
Pratik Raj <pratikraj26@gmail.com>
2017-11-02 12:15:31 +0530

    Design complete for home page

--- main/client/app/main/main.controller.js
+++ main/client/app/main/main.controller.js
@@ -1,29 +1,29 @@
 'use strict';
 
 angular.module('takhshilaApp')
-  .controller('MainCtrl', function ($rootScope, $scope, $state, $http) {
-    $rootScope.isLoading = false;
-    $rootScope.search = {
-    	topic: null,
-    	level: 'Basic'
-    }
-    $scope.loadingResults = false;
-    $rootScope.processSearch = function(evt){
-    	evt.preventDefault();
-    	evt.stopPropagation();
-    	if($rootScope.search.topic !== null && $rootScope.search.level !== null){
-    		$state.go('search', $rootScope.search);
-    	}
-    }
+.controller('MainCtrl', function ($rootScope, $scope, $state, $http) {
+	$rootScope.isLoading = false;
+	$rootScope.search = {
+		topic: null,
+		level: 'Basic'
+	}
+	$scope.loadingResults = false;
+	$rootScope.processSearch = function(evt){
+		evt.preventDefault();
+		evt.stopPropagation();
+		if($rootScope.search.topic !== null && $rootScope.search.level !== null){
+			$state.go('search', $rootScope.search);
+		}
+	}
 
-    $scope.getTopicList = function(searchTerm) {
-      $scope.loadingResults = true;
-      return $http.get('/api/v1/topics/search/'+searchTerm).then(function(response){
-        $scope.loadingResults = false;
-        return response.data.map(function(item){
-          return item.topicName;
-        });
-      });
-    };
+	$scope.getTopicList = function(searchTerm) {
+		$scope.loadingResults = true;
+		return $http.get('/api/v1/topics/search/'+searchTerm).then(function(response){
+			$scope.loadingResults = false;
+			return response.data.map(function(item){
+				return item.topicName;
+			});
+		});
+	};
 
-  });
+});
