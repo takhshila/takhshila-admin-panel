5e434ab7c9188a03134b1f4ab2c505aea95b0664
takhshila@gmail.com <takhshila@gmail.com>
2017-12-19 11:53:33 +0530

    Added functonality to delete or publish topic from admin panel

--- main/client/app/admin/admin-topics/admin-topics.controller.js
+++ main/client/app/admin/admin-topics/admin-topics.controller.js
@@ -12,7 +12,7 @@ angular.module('takhshilaApp')
   	var dataPerPage = 20;
   	
   	$scope.getTopics = function(){
-		$http.get('/api/v1/topics/?page=' + currentPage + '&perPage=' + dataPerPage)
+		return $http.get('/api/v1/topics/?page=' + currentPage + '&perPage=' + dataPerPage)
 		.then(function(response){
 			$rootScope.isLoading = false;
 			$scope.topics = response.data;
@@ -34,7 +34,6 @@ angular.module('takhshilaApp')
 
     $scope.addTopic = function(addTopicForm) {
       $scope.addTopicError = false;
-
       if(addTopicForm.$invalid){
         var el = angular.element("[name='" + addTopicForm.$name + "']").find('.ng-invalid:visible:first');
         var elName = el[0].name;
@@ -48,26 +47,58 @@ angular.module('takhshilaApp')
         	"topicName": $scope.addTopicFormData.topicName
         })
         .then(function(data){
-			$scope.addingTopic = false;
-			if(data.status === 201){
-				$scope.addTopicFormData.topicName = null;
-			}else{
-				$scope.addTopicError = true;
-				$scope.addTopicErrorMessage = data.error;
-			}
+    			$scope.addingTopic = false;
+    			if(data.status === 201){
+    				$scope.addTopicFormData.topicName = null;
+    			}else{
+    				$scope.addTopicError = true;
+    				$scope.addTopicErrorMessage = data.error;
+    			}
         }, function(err){
-        	console.log(err);
-			$scope.addingTopic = false;
-			for(var error in err.errors){
-				addTopicForm[error].$valid = false;
-				addTopicForm[error].$invalid = true;
-				addTopicForm[error].$error.serverError = true;
-				addTopicForm[error].$error.errorMessage = err.data.errors[error].message;
-				angular.element("[name='" + addTopicForm.$name + "'] [name='" + error + "']").focus();
-			}
+    			$scope.addingTopic = false;
+    			for(var error in err.errors){
+    				addTopicForm[error].$valid = false;
+    				addTopicForm[error].$invalid = true;
+    				addTopicForm[error].$error.serverError = true;
+    				addTopicForm[error].$error.errorMessage = err.data.errors[error].message;
+    				angular.element("[name='" + addTopicForm.$name + "'] [name='" + error + "']").focus();
+    			}
         })
       }
     };
+
+    $scope.deleteTopic = function(topicID){
+      $scope.deletingTopic = true;
+      $http.delete('/api/v1/topics/' + topicID)
+      .then(function(){
+        $scope.getTopics()
+        .then(function(){
+          $scope.deletingTopic = false;
+        });
+      }, function(err){
+        console.log(err);
+        $scope.deletingTopic = false;
+      });
+    }
+
+    $scope.publishTopic = function(topicID){
+      $scope.publishingTopic = true;
+      $http.put('/api/v1/topics/' + topicID, {
+        active: true
+      })
+      .then(function(){
+        $scope.publishingTopic = false;
+        for(var i = 0; i < $scope.topics.length; i++){
+          if($scope.topics[i]._id === topicID){
+            $scope.topics[i].active = true;
+            break;
+          }
+        }
+      }, function(err){
+        console.log(err);
+        $scope.publishingTopic = false;
+      });
+    }
 
     $rootScope.$watch('loggedIn', function(status){
       if(status === true){
